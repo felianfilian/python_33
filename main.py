e4cbfa1a3469cd5dcbd45f8ed45558917524a7b4
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-28 21:19:09 +0100

    get specific data

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import requests
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
 
-data = response.json()
-print(data)
+longitude = response.json()["iss_position"]["longitude"]
+latitude = response.json()["iss_position"]["latitude"]
+print(f"{longitude} - {latitude}")
