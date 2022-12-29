92f6e121120f44617635d7b23b7600404b29da19
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-29 20:30:17 +0100

    rearrange project

---
+++ api_test.py
@@ -0,0 +1,13 @@
+import requests
+
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+
+longitude = response.json()["iss_position"]["longitude"]
+latitude = response.json()["iss_position"]["latitude"]
+print(f"{longitude} - {latitude}")
+
+timestamp = response.json()["timestamp"]
+print(timestamp)
+
--- main.py
+++ main.py
@@ -1,15 +1,2 @@
-import requests
+import api_test
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-
-longitude = response.json()["iss_position"]["longitude"]
-latitude = response.json()["iss_position"]["latitude"]
-print(f"{longitude} - {latitude}")
-
-timestamp = response.json()["timestamp"]
-print(timestamp)
-
-
-response02 = requests.get(url="https://api.open-meteo.com/v1/dwd-icon?latitude=52.52&longitude=13.41&hourly=temperature_2m")
-elevation = response.json()["elevation"]
