40bb57e75fec860682164a2bb2661a5adb044c3f
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-29 21:03:30 +0100

    get api data

--- kayne_quotes.py
+++ kayne_quotes.py
@@ -1,6 +1,9 @@
 from tkinter import *
+import requests
 
 def get_quote():
+    response = requests.get("https://api.kanye.rest")
+    response.raise_for_status()
     canvas.itemconfig(quote_text, text="Yo man")
 
 window = Tk()
