cf5173038ee5fa53b344e9312dd1ebaad241552b
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-30 05:04:45 +0100

    advanced string splitting

--- sunrise_sunset.py
+++ sunrise_sunset.py
@@ -1,9 +1,12 @@
 import requests
 
-response = requests.get("https://api.sunrise-sunset.org/json")
+parameters = {
+    "formatted": 0
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 
 sunrise = response.json()["results"]["sunrise"]
-
-print(sunrise)
+print(sunrise.split("T")[1].split(":")[0])
 
