e95141a9c13ffc6c720d8e1388e932e26f976fb9
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-31 21:47:31 +0100

    test the comparsion

--- iss_pos.py
+++ iss_pos.py
@@ -6,17 +6,20 @@ MY_LONG = -0.1277
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
 
-longitude = response.json()["iss_position"]["longitude"]
-latitude = response.json()["iss_position"]["latitude"]
+longitude = 0
+latitude = 51
+
+#longitude = response.json()["iss_position"]["longitude"]
+#latitude = response.json()["iss_position"]["latitude"]
 print(f"{longitude} - {latitude}")
 
 longitude = float(longitude)
 latitude = float(latitude)
-print(type(longitude))
-print(type(MY_LONG))
 
-if (MY_LONG > longitude - 5 and MY_LONG < longitude + 5) and (MY_LONG > latitude - 5 and MY_LONG < latitude + 5):
+if (MY_LONG > longitude - 5 and MY_LONG < longitude + 5) and (MY_LAT > latitude - 5 and MY_LAT < latitude + 5):
     print("its there")
+else:
+    print("its NOT there")
 
 timestamp = response.json()["timestamp"]
 print(timestamp)
