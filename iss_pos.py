d6c34341d49ea55f1d35c30c2b259ec409dcdf38
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-31 21:59:56 +0100

    iss overhead function

--- iss_pos.py
+++ iss_pos.py
@@ -3,23 +3,21 @@ import requests
 MY_LAT = 51.5073
 MY_LONG = -0.1277
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
+def is_iis_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
 
-longitude = 0
-latitude = 51
+    longitude = response.json()["iss_position"]["longitude"]
+    latitude = response.json()["iss_position"]["latitude"]
+    print(f"{longitude} - {latitude}")
 
-#longitude = response.json()["iss_position"]["longitude"]
-#latitude = response.json()["iss_position"]["latitude"]
-print(f"{longitude} - {latitude}")
+    longitude = float(longitude)
+    latitude = float(latitude)
 
-longitude = float(longitude)
-latitude = float(latitude)
-
-if MY_LONG - 5 < longitude < MY_LONG + 5  and MY_LAT - 5 < latitude < MY_LAT + 5:
-    print("its there")
-else:
-    print("its NOT there")
+    if MY_LONG - 5 < longitude < MY_LONG + 5  and MY_LAT - 5 < latitude < MY_LAT + 5:
+        return True
+    else:
+        return False
 
 timestamp = response.json()["timestamp"]
 print(timestamp)
