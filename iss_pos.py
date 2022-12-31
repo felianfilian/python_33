d8132286184ff042cc97275c1a49083961264a0f
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-31 05:12:25 +0100

    position check test

--- iss_pos.py
+++ iss_pos.py
@@ -1,7 +1,7 @@
 import requests
 
-MY_LAT = 51.507351
-MY_LONG = -0.127758
+MY_LAT = 51.5073
+MY_LONG = -0.1277
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -9,6 +9,9 @@ response.raise_for_status()
 longitude = response.json()["iss_position"]["longitude"]
 latitude = response.json()["iss_position"]["latitude"]
 print(f"{longitude} - {latitude}")
+
+if (MY_LONG > longitude - 5 and MY_LONG < longitude + 5) and (MY_LONG > latitude - 5 and MY_LONG < latitude + 5):
+    print("its there")
 
 timestamp = response.json()["timestamp"]
 print(timestamp)
